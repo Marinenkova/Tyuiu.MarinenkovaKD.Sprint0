4f6f2206a8295ff6f7d9243ecb6289b4bb17324a
Tyuiu.MarinenkovaKD@outlook.com <marinenkova05@gmail.com>
2023-09-15 13:00:58 +0500

    Task4

---
+++ Tyuiu.MarinenkovaKD.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MarinenkovaKD.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MarinenkovaKD.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64421e5f-6ea9-49d7-a317-144c4d62d634")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MarinenkovaKD.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MarinenkovaKD.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.MarinenkovaKD.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.ReadKey();
+        }
+    }
+}
