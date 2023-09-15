9b922a63eaf181360721a9be4e0085cdb2f99abc
Tyuiu.MarinenkovaKD@outlook.com <marinenkova05@gmail.com>
2023-09-15 13:36:42 +0500

    Task4

--- Tyuiu.MarinenkovaKD.Sprint0.Task4.V0/Program.cs
+++ Tyuiu.MarinenkovaKD.Sprint0.Task4.V0/Program.cs
@@ -12,6 +12,9 @@ namespace Tyuiu.MarinenkovaKD.Sprint0.Task4.V0
         static void Main(string[] args)
         {
             Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            Console.WriteLine(DataService.Division(5, 5));
             Console.ReadKey();
         }
     }
